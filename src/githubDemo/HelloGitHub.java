e41de9ac6c8f9e4c292c721246cf37811f72b3ed
JamilAhmedQA <jamilctrl@gmail.com>
2022-02-05 17:47:08 -0700

    02052022

--- src/githubDemo/HelloGitHub.java
+++ src/githubDemo/HelloGitHub.java
@@ -1,10 +1,7 @@
 package githubDemo;
 
 public class HelloGitHub {
-
 	public static void main(String[] args) {
-		System.out.println("Testing demo");
-
+		System.out.println("I know how to create a separate remote repository in Github and can push my project GithubDemo, without help of anyone");
 	}
-
 }
